12d97b293b3d7e683e2f42b70ad8b14a927cd453
juliusbuckley <juliusbuckley@gmail.com>
2016-06-25 12:22:53 -0700

    Completed searchYouTube and App prompts.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,3 +10,23 @@ var Search = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.Search = Search;
+
+// search: list
+// returns collection of search results matching query parameters specified in API request
+// compare search to results
+
+/* {
+  "kind": "youtube#searchListResponse",
+  "etag": etag,
+  "nextPageToken": string,
+  "prevPageToken": string,
+  "regionCode": string,
+  "pageInfo": {
+    "totalResults": integer,
+    "resultsPerPage": integer
+  },
+  "items": [
+    search Resource
+  ]
+}
+*/
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${options.key}&videoEmbeddable=true&q=${options.query}&maxResults=${options.max}&type=video`,
+    type: 'GET',
+    success: data => {
+      callback(data.items);
+    },
+    error: data => {
+      console.log('failed to connect to youtube');
+    }
+  });
 };
 
+searchYoutube({key: 'AIzaSyDv4gvT5m8dC_qPj_LGkrh15pyX4QdN8d0', query: 'cats', max: 10}, function(data) {
+  videoData.push(data);
+  console.log(data);
+});
+    
 window.searchYouTube = searchYouTube;
